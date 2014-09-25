a0f823cc90685f408c0e7e17fe0684524d73175b
Chung Kang <cykang@central.uh.edu>
2014-09-25 09:14:42 -0500

    ugh

--- Gruntfile.js
+++ Gruntfile.js
@@ -104,6 +104,7 @@ module.exports = function(grunt) {
   grunt.loadNpmTasks('grunt-contrib-uglify');
   grunt.loadNpmTasks('grunt-contrib-watch');
   grunt.loadNpmTasks('grunt-contrib-connect');
+  grunt.loadNpmTasks('grunt-heroku-deploy');
  
   // define the tasks
   grunt.registerTask(
