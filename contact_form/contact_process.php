1d23194a859e10412a649c0bb784631de15cd5fb
Chung Kang <cykang@central.uh.edu>
2015-01-05 14:16:51 -0600

    try email functionality

--- contact_form/contact_process.php
+++ contact_form/contact_process.php
@@ -1,68 +1,67 @@
 <?php
 
-include dirname(dirname(__FILE__)).'/mail.php';
+require_once $_SERVER['DOCUMENT_ROOT'] . '/libs/swift-5.0.1/lib/swift_required.php';
+#require $_SERVER['DOCUMENT_ROOT'] . '/libs/sendgrid-php/SendGrid_loader.php';
+$user = getenv("SENDGRID_USERNAME");
+$pass = getenv("SENDGRID_PASSWORD");
 
-error_reporting (E_ALL ^ E_NOTICE);
 
 $post = (!empty($_POST)) ? true : false;
 
-if($post)
-{
-include 'email_validation.php';
+error_reporting (E_ALL ^ E_NOTICE);
 
-$name = stripslashes($_POST['name']);
-$email = trim($_POST['email']);
-$subject = stripslashes($_POST['subject']);
-$message = stripslashes($_POST['message']);
+if ($post) {
+  $sendgrid = new SendGrid($user, $pass);  
+
+  include 'email_validation.php';
+
+  $name = stripslashes($_POST['name']);
+  $email = trim($_POST['email']);
+  $subject = stripslashes($_POST['subject']);
+  $message = stripslashes($_POST['message']);
+
+  $error = '';
+
+  // Check name
+  if(!$name) {
+    $error .= 'Please enter your name.<br />';
+  }
+
+  // Check email
+
+  if(!$email) {
+    $error .= 'Please enter an e-mail address.<br />';
+  }
+
+  if($email && !ValidateEmail($email)) {
+    $error .= 'Please enter a valid e-mail address.<br />';
+  }
+
+  // Check message (length)
+
+  if(!$message || strlen($message) < 10) {
+    $error .= "Please enter your message. It should have at least 10 characters.<br />";
+  }
+
+  if(!$error) {
+    $mail = new SendGrid\Mail();
+
+    $mail->addTo('chung.kang@gmail.com')->
+        setFrom($email)->
+        setFromName($name)->
+        setSubject($subject)->
+        setText($message)->
+        setHtml($message);
+
+    if($sendgrid->smtp->send($mail)){
+      echo 'OK';
+    };
 
 
-$error = '';
+  } else {
+    echo '<div class="notification_error">'.$error.'</div>';
+  }
 
-// Check name
-
-if(!$name)
-{
-$error .= 'Please enter your name.<br />';
-}
-
-// Check email
-
-if(!$email)
-{
-$error .= 'Please enter an e-mail address.<br />';
-}
-
-if($email && !ValidateEmail($email))
-{
-$error .= 'Please enter a valid e-mail address.<br />';
-}
-
-// Check message (length)
-
-if(!$message || strlen($message) < 10)
-{
-$error .= "Please enter your message. It should have at least 10 characters.<br />";
-}
-
-
-if(!$error)
-{
-$mail = mail(CONTACT_FORM, $subject, $message,
-     "From: ".$name." <".$email.">\r\n"
-    ."Reply-To: ".$email."\r\n"
-    ."X-Mailer: PHP/" . phpversion());
-
-
-if($mail)
-{
-echo 'OK';
-}
-
-}
-else
-{
-echo '<div class="notification_error">'.$error.'</div>';
-}
 
 }
 ?>
